98ac0e55f0afa8183396b279f52362fa03052730
shadreza <shadreza100@gmail.com>
2023-02-15 22:03:14 +0600

    relationship ready

--- database/migrations/2023_02_15_151451_create_posts_table.php
+++ database/migrations/2023_02_15_151451_create_posts_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->text('body');
+            $table->integer('user_id')->unsigned()->nullable()->index();
         });
     }
 
