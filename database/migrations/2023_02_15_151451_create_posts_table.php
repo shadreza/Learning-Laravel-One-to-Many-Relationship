96489d4fbfac44918a653af27929df8a1efc5719
shadreza <shadreza100@gmail.com>
2023-02-15 23:01:54 +0600

    create done

--- database/migrations/2023_02_15_151451_create_posts_table.php
+++ database/migrations/2023_02_15_151451_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('body');
             $table->integer('user_id')->unsigned()->nullable()->index();
+            $table->timestamps();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| CRUD
+|--------------------------------------------------------------------------
+|
+*/
+
+
+// inserting user [create]
+Route::get('create-user/{name}', function ($name) {
+    $user = new User(['name' => $name, 'email' => $name . '@gmail.com', 'password' => bcrypt('123412')]);
+    return $user->save();
+});
+
+
+// inserting post [create]
+Route::get('create-post/{user_id}/{post_name}', function ($user_id, $post_name) {
+    $user = User::findOrFail($user_id);
+    $user->posts()->save(new Post(['title' => $post_name, 'body' => $post_name . " many more to come"]));
 });
